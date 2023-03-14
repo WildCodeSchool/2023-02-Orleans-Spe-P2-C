a24f0c52c87946a221a03263731f2d7605efbae7
SILVESTRI_GREGORY <gregory.silvestri45@gmail.com>
2023-03-14 23:53:12 +0100

    dialog

--- src/sceneFinal.js
+++ src/sceneFinal.js
@@ -23,10 +23,46 @@ function preload() {
 let hero
 let princess
 let dialog
+let dialogueText
 function create() {
     this.add.image(800, 400, 'pool');
     hero = this.add.image(350, 605, 'hero').setScale(1.2);
     princess = this.add.image(1500, 605, 'princess').setScale(1.2);
     dialog = this.add.image(950, 820, 'dialog');
+
+    // Ajout du texte de la boîte de dialogue
+    dialogueText = this.add.text(500, 750, "Hey", {
+        fontSize: "60px",
+        color: "#fff",
+        wordWrap: { width: 1000, useAdvancedWrap: true },
+    });
+
+    // Texte à afficher dans la boîte de dialogue
+    let dialogue = [
+        "Princess: Bonjour Hero !",
+        "....",
+        "Princess: Aujourd'hui est le jour de ton avainement !",
+        "...",
+    ];
+
+    // Index du texte actuellement affiché
+    let dialogueIndex = 0;
+
+    // Ajout d'un écouteur d'événements pour la touche "X"
+    this.input.keyboard.on("keydown-X", () => {
+        // S'il reste du texte à afficher
+        if (dialogueIndex < dialogue.length) {
+            // Affiche le texte suivant
+            dialogueIndex++;
+            dialogueText.setText(dialogue[dialogueIndex]);
+        } else {
+            // Ferme la boîte de dialogue
+            dialogueText.setText("");
+            dialogueIndex = 0;
+        }
+
+    });
+
+    dialogueText.setText(dialogue[dialogueIndex]);
 
 }
