21c06c5437d14bf186f1fd9e3889d46a6ba7bcd5
Chloebert <berthier.chloe@wanadoo.fr>
2023-03-15 15:12:08 +0100

    again

--- src/game.js
+++ src/game.js
@@ -4,6 +4,8 @@ import SceneThree from './scenes/scene-three.js';
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-game',
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
     width: 1280,
     height: 720,
     backgroundColor: '#2d2d2d',
